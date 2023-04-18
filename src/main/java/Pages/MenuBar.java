f79547a2c20ef08bf0bd26a82116b5725e59519c
sahanif <sahani@squaretrade.com>
2023-04-18 08:53:39 +0500

    updated menu bar page file to add more test cases and report generated

--- src/main/java/Pages/MenuBar.java
+++ src/main/java/Pages/MenuBar.java
@@ -3,13 +3,34 @@ package Pages;
 import Util.BaseTests;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MenuBar extends BaseTests {
+
+    WebDriverWait wait;
+    private final By hamburgerMenu = By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]");
+    private final By inboxPausedButton = By.xpath("(//android.widget.ImageView[@content-desc=\"pause now\"])[1]");
+
+    public MenuBar(){
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
     public void scrollDownToSendFeedback() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
-        Thread.sleep(2000);
-        //For scroll down
-        driver.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).index(0)).scrollIntoView(new UiSelector().resourceId(\"com.baydin.boomerang:id/drawer_feedback\"))")).click(); //scroll down to the element and click
+        // For scroll down till the Send feedback element is found
+        String uiAutomatorTextForSendFeedback = "new UiScrollable(new UiSelector().scrollable(true).index(0)).scrollIntoView(new UiSelector().resourceId(\"com.baydin.boomerang:id/drawer_feedback\"))";
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator(uiAutomatorTextForSendFeedback)).click();
+    }
+
+    public void openMenuBar(){
+        waitForElementPresent(hamburgerMenu);
+        driver.findElement(hamburgerMenu).click();
+    }
+
+    public void clickInboxPauseButton(){
+        waitForElementPresent(inboxPausedButton);
+        driver.findElement(inboxPausedButton).click();
     }
 }
