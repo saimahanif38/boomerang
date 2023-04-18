66561772f043f94115c34138796ed2b30c86e261
sahanif <sahani@squaretrade.com>
2023-04-18 08:52:31 +0500

    update BaseTests file with setEmailAndPassword method

--- src/main/java/Util/BaseTests.java
+++ src/main/java/Util/BaseTests.java
@@ -1,31 +1,48 @@
 package Util;
+
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseTests {
 
     public static AppiumDriver driver;
+    public static String email = "", password = "";
+    protected static String projectPath = System.getProperty("user.dir");
+    public static String path = projectPath + "/resources/";
     WebDriverWait wait;
 
     public static void setup() throws MalformedURLException {
-        clearBrowser();
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("platformName", "android");
         cap.setCapability("udid", "emulator-5554");
         cap.setCapability("platformVersion", "9");
         cap.setCapability("deviceName", "emulator-5554");
-        cap.setCapability("appPackage","com.baydin.boomerang");
-        cap.setCapability("app","C:/Users/saimahanif/Downloads/boomerangandroid-prod-release.apk");
+        cap.setCapability("appPackage", "com.baydin.boomerang");
+        cap.setCapability("app", "C:/Users/saimahanif/Downloads/boomerangandroid-prod-release.apk");
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver(url, cap);
+    }
+
+    public static void setEmailAndPassword() throws IOException {
+        FileReader reader = new FileReader(path + "email.properties");
+        Properties prop = new Properties();
+        prop.load(reader);
+        email = prop.getProperty("email");
+        password = prop.getProperty("password");
+    }
+
+    public static void tearDown() {
+        driver.quit();
     }
 
     public void waitForElementPresent(By locator) {
@@ -37,16 +54,5 @@ public class BaseTests {
             e.printStackTrace();
 
         }
-    }
-
-    public static void clearBrowser() throws MalformedURLException {
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-        cap.setCapability(MobileCapabilityType.CLEAR_SYSTEM_FILES, "true");
-        cap.setCapability("fullReset", "true");
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-    }
-    public static void tearDown() {
-        driver.quit();
     }
 }
